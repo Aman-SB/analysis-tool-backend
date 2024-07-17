6895ccee88bcfb6528bb1fb1264559108779c178
Aman Bisht <bishtaman60@gmail.com>
2024-07-17 18:56:55 +0530

    first commit

---
+++ src/main/java/com/aman/ananlysis_tool/AnanlysisToolApplication.java
@@ -0,0 +1,13 @@
+package com.aman.ananlysis_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnanlysisToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnanlysisToolApplication.class, args);
+	}
+
+}
